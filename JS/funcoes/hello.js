0dfbc339edf0d2ae013e0a483df9a6254f409bdc
rebortoliero <57117344+rebortoliero@users.noreply.github.com>
2019-10-28 22:28:11 -0300

    Add files via upload

---
+++ JS/funcoes/hello.js
@@ -0,0 +1,9 @@
+/**
+ * Função simples - Evento de função;
+ * @author Renato Bortoliero
+ */
+
+ function helloWorld(){
+    nome = prompt ("Qual é o seu nome?")
+    alert ('Hello ' + nome)
+   }
---
+++ JS/vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * Exemplo de Vinulação de um documento JS no HTML 
+ *  @author Renato Bortoliero
+ */
+
+document.write('<h3>' + 'Hello World!' + '</h3>');
