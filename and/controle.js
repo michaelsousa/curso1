8b4b8e1d898fbf408faf911cfb744ec787a45e1c
rebortoliero <57117344+rebortoliero@users.noreply.github.com>
2019-11-06 14:57:48 -0300

    And corrigido
    Exercício aula 3

--- and/controle.js
+++ and/controle.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *  
  * 
  */
 
@@ -7,48 +7,40 @@ let pmt, pmm
 let sw1 = false
 let sw2 = false
 
-function broken(pmm) {
-if (pmm==1){
-    document.getElementById("lamp").src = "broken.jpg"
-    function sw(pmt) {
-        if (pmt == 1 && sw1 == false) {
-            document.getElementById("sw1").src = "swon.png"
-            sw1 = true;
-        } else if (pmt == 1 && sw1 == true) {
-            document.getElementById("sw1").src = "swoff.png"
-            sw1 = false
-        } else if (pmt == 2 && sw2 == false) {
-            document.getElementById("sw2").src = "swon.png"
-            sw2 = true;
-        } else {
-            document.getElementById("sw2").src = "swoff.png"
-            sw2 = false
-        }}
+function reparar() {
+    document.getElementById("Queimada").src = "off.jpg"
+    document.getElementById("Queimada").id = 'lamp'
 
-} else {
-    function sw(pmt) {
-        if (pmt == 1 && sw1 == false) {
-            document.getElementById("sw1").src = "swon.png"
-            sw1 = true;
-        } else if (pmt == 1 && sw1 == true) {
-            document.getElementById("sw1").src = "swoff.png"
-            sw1 = false
-        } else if (pmt == 2 && sw2 == false) {
-            document.getElementById("sw2").src = "swon.png"
-            sw2 = true;
-        } else {
-            document.getElementById("sw2").src = "swoff.png"
-            sw2 = false
-        }
-    }
-    
-
-        if (sw2 && sw1) {
-            document.getElementById("lamp").src = "on.jpg"
-        }
-        else {
-            document.getElementById("lamp").src = "off.jpg"
-        }
-    }
- 
 }
+function broken() {
+    document.getElementById("lamp").src = "broken.jpg"
+    document.getElementById("lamp").id = 'Queimada'
+}
+
+function sw(pmt) {
+    if (pmt == 1 && sw1 == false) {
+        document.getElementById("sw1").src = "swon.png"
+        sw1 = true;
+    } else if (pmt == 1 && sw1 == true) {
+        document.getElementById("sw1").src = "swoff.png"
+        sw1 = false
+    } else if (pmt == 2 && sw2 == false) {
+        document.getElementById("sw2").src = "swon.png"
+        sw2 = true;
+    } else {
+        document.getElementById("sw2").src = "swoff.png"
+        sw2 = false
+    }
+
+    if (sw2 && sw1) {
+        document.getElementById("lamp").src = "on.jpg"
+    }
+    else {
+        document.getElementById("lamp").src = "off.jpg"
+    }
+}
+
+
+
+
+
